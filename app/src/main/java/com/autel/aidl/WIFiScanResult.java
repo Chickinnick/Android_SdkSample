32a4d71fc3d64b915afa3d88f2b1e550ad54ac66
Macpro <fengyinhua@autelrobotics.cn>
2021-02-19 11:11:33 +0800

    update scan wifi

--- app/src/main/java/com/autel/aidl/WIFiScanResult.java
+++ app/src/main/java/com/autel/aidl/WIFiScanResult.java
@@ -11,13 +11,32 @@ public class WIFiScanResult implements Parcelable {
      * The network name.
      */
     public String SSID;
+    /**
+     * The address of the access point.
+     */
+    public String BSSID;
+    /*
+     * This field is equivalent to the |flags|, rather than the |capabilities| field
+     * of the per-BSS scan results returned by WPA supplicant. See the definition of
+     * |struct wpa_bss| in wpa_supplicant/bss.h for more details.
+     */
+    /**
+     * Describes the authentication, key management, and encryption schemes
+     * supported by the access point.
+     */
+    public String capabilities;
 
-    public WIFiScanResult(String ssid){
+
+    public WIFiScanResult(String ssid,String bSSID,String capabilities){
         this.SSID = ssid;
+        this.BSSID = bSSID;
+        this.capabilities = capabilities;
     }
 
     protected WIFiScanResult(Parcel in) {
         SSID = in.readString();
+        BSSID = in.readString();
+        capabilities = in.readString();
     }
 
     public static final Creator<WIFiScanResult> CREATOR = new Creator<WIFiScanResult>() {
@@ -40,5 +59,7 @@ public class WIFiScanResult implements Parcelable {
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(SSID);
+        dest.writeString(BSSID);
+        dest.writeString(capabilities);
     }
 }
